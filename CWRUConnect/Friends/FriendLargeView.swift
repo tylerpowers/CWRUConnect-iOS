d9d0238e6bfabb64e81f93e380e854ba9430ad13
tylerpowers <tylerpowers4@gmail.com>
2026-04-07 01:44:21 -0400

    Updated UI, still barebones though.

--- CWRUConnect/Friends/FriendLargeView.swift
+++ CWRUConnect/Friends/FriendLargeView.swift
@@ -11,18 +11,38 @@ struct FriendLargeView: View {
     @State var friend: User
     var body: some View {
         VStack {
-            FriendSmallView(friend: friend)
-                .padding()
-            Spacer()
+            AsyncImage(url: URL(string: friend.image_link)) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 200, height: 200)
+            Text(friend.nickname)
+                .font(.largeTitle)
+            Text(friend.pronunciation)
+                .font(.caption)
+                .italic()
             Text(friend.minibio)
-                .padding()
-            Spacer()
+            HStack {
+                Text(friend.pronouns)
+                Spacer()
+                Text("MAJOR")
+                Spacer()
+                Text(friend.graduation_year.description)
+            }
+            .padding()
+            HStack {
+                Text(friend.hometown)
+                Spacer()
+                Text(friend.nationality)
+            }
+            .padding()
         }
     }
 }
 
 #Preview {
-//    FriendLargeView(
-//        friend: .constant(Friend(name: "Alan S.", email: "ams2299@case.edu", headline: "Computer Science", bio: "Hey! My name is Alan Slice! I'm originally from Maine, but spent most of my upbringing in the DMV area (Vienna, VA for the curious!). My hobbies include eating/making/talking about pizza, playing ukulele, and haunting people. HAHAHA! I am going to haunt you forever!", image: "cat"))
-//    )
+    FriendLargeView(
+        friend: User(userid: 999, name: "Alan Slice", nickname: "Alan S.", caseid: "axs999", pronouns: "he/him", graduation_year: 2029, hometown: "Kentucky, AL", nationality: "American", image_link: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpeople.com%2Fthmb%2FLdqcEIO-al1_9Rxm6u_lSpgSHek%3D%2F2000x1417%2Ffilters%3Afill(auto%2C1)%2Fcam-newton-2000-022cc4221f514ecf85012a3598e6faea.jpg&f=1&nofb=1&ipt=93528b5a5db43c4e84c597fa5befb3e201341f3fe7beae45e7a8d71cade69385", pronunciation: "Al-uhn S.", minibio: "I'm Alan, I love skating and haunting!", fact: "I haunt...", is_public_leaderboard: true, note: "bestie", starred: false, matched_at: "Location of")
+    )
 }
--- CWRUConnect/Friends/FriendSmallView.swift
+++ CWRUConnect/Friends/FriendSmallView.swift
@@ -11,27 +11,28 @@ struct FriendSmallView: View {
     @State var friend: User
     var body: some View {
         HStack {
-//            if let friend_img = friend.image_link {
-//                Image(friend_img)
-//                    .resizable()
-//                    .clipShape(Circle())
-//                    .frame(width: 100, height: 100)
-//                    .padding()
-//            }
-            Image(systemName: "person")
-            Spacer()
+            AsyncImage(url: URL(string: friend.image_link)) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+            .padding()
             VStack {
                 Text(friend.nickname)
                     .font(.title)
-                Text(friend.minibio)
+                Text(friend.matched_at)
             }
+            .padding()
+            Spacer()
         }
         .padding()
     }
 }
 
 #Preview {
-//    FriendSmallView(
-//        friend: .constant(Friend(name: "Alan S.", email: "ams2299@case.edu", headline: "Computer Science", bio: "", image: "cat"))
-//    )
+    FriendSmallView(
+        friend: User(userid: 999, name: "Alan Slice", nickname: "Alan S.", caseid: "axs999", pronouns: "he/him", graduation_year: 2029, hometown: "Kentucky, AL", nationality: "American", image_link: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpeople.com%2Fthmb%2FLdqcEIO-al1_9Rxm6u_lSpgSHek%3D%2F2000x1417%2Ffilters%3Afill(auto%2C1)%2Fcam-newton-2000-022cc4221f514ecf85012a3598e6faea.jpg&f=1&nofb=1&ipt=93528b5a5db43c4e84c597fa5befb3e201341f3fe7beae45e7a8d71cade69385", pronunciation: "Al-uhn S.", minibio: "I'm Alan, I love skating and haunting!", fact: "I haunt...", is_public_leaderboard: true, note: "bestie", starred: false, matched_at: "Location of")
+    )
 }
